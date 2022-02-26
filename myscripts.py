c78fbb6fe59b1373b021135a8ec278aaa6bb5861
ThomasK13 <65359305+ThomasK13@users.noreply.github.com>
2022-02-26 15:18:49 +0530

    updated

--- myscripts.py
+++ myscripts.py
@@ -1,9 +1,29 @@
-
-# getting the parent directory 
 import os
 def get_parent_path():
+    # getting the parent directory 
 	cwd = os.getcwd()
 	parent_dir = os.path.abspath(os.path.join(cwd,os.pardir))
 	return parent_dir
 
-
+def is_monotonic(ar):
+    # whether an array is monotonic_increasing or not 
+    for j in range(len(ar)-1):
+        if ar[j]>ar[j+1]:
+            return False
+    else:
+        return True
+def unordered_counter(ar1):
+    # how many elements are unorderd in an array
+    counter = 0
+    order = is_monotonic(ar1)
+    while not order:
+        for i in range(len(ar1)):
+            if i<len(ar1)-1 and ar1[i]>ar1[i+1]:
+                counter+=1
+                ar1[i+1],ar1[i] = ar1[i],ar1[i+1]
+                print(ar1)
+                break
+            order = is_monotonic(ar1)
+    else:
+        return counter
+        
